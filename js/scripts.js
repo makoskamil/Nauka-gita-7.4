54b5e341668564ad8153cea0b08d01429ea143cc
Kamil Makoś <makos.kamil@o2.pl>
2018-03-08 09:49:42 +0100

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+
+/* 
+    block comment
+*/ 
+
+// another comment
+console.log('Hello, ' + name);
+
+var points = 1;
+
+console.log( 'The current value of points is: ', points ); // 1
+console.log( 'The current value of points is: ', points++ ); // 1
+console.log( 'The current value of points is: ', points ); // 2
